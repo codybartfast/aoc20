530dd6edee2c033091e6eb27d520d44b3b04fc3a
John Donnellan <john@codybartfast.com>
2020-12-01 05:18:58 +0000

    Day 1 - used

--- Day01.fs
+++ Day01.fs
@@ -1,17 +1,22 @@
 open System
 open System.Text.RegularExpressions
 
-let inline toChars (str: string) = str.ToCharArray()
+let inline toChars (str: string) = str.ToCharArray() 
 
-let lines = System.IO.File.ReadAllLines("Day01.txt") |> Array.toList
-let parse (ln: string) = ln //.Split(' ')
-let input = lines |> List.map parse
+let expns = System.IO.File.ReadAllLines("Day01.txt") |> Array.map int
+let lst = expns.Length - 1
 
 let part1 =
-    input
+    [ for i in 0 .. lst do for j in i + 1 .. lst do
+        let a, b = expns.[i], expns.[j]
+        if a + b = 2020 then
+            (a * b) ].Head
 
 let part2 =
-    "?"
+    [ for i in 0 .. lst do for j in i + 1 .. lst do for k in j + 1 .. lst do
+        let a, b, c = expns.[i], expns.[j], expns.[k]
+        if a + b + c = 2020 then
+            (a * b * c) ].Head
 
 [<EntryPoint>]
 let main _ =

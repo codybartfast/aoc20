ce38eccf15c1f47c9a7005143b35eaf854f4977e
John Donnellan <john@codybartfast.com>
2020-12-01 06:01:00 +0000

    🗟

--- Day01.fs
+++ Day01.fs
@@ -1,17 +1,18 @@
-open System
-open System.Text.RegularExpressions
+let expns =
+    System.IO.File.ReadAllLines("Day01.txt")
+    |> Array.map int |> Array.toList
 
-let inline toChars (str: string) = str.ToCharArray()
+let tuples lst n =
+    let rec tuples n tpls =
+        if n = 0 then tpls else
+            tuples (n - 1) (tpls |> Seq.collect (fun tpl ->
+                        lst |> Seq.map (fun e -> e::tpl)))
+    tuples n (Seq.singleton [])
 
-let lines = System.IO.File.ReadAllLines("Day01.txt") |> Array.toList
-let parse (ln: string) = ln //.Split(' ')
-let input = lines |> List.map parse
+let entries = tuples expns >> Seq.filter (List.sum >> ((=) 2020)) >> Seq.head
 
-let part1 =
-    input
-
-let part2 =
-    "?"
+let part1 = entries 2 |> List.reduce (*)
+let part2 = entries 3 |> List.reduce (*)
 
 [<EntryPoint>]
 let main _ =

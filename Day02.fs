558fcc657eb77bd02f82f8b2d4f737f46dcc726c
John Donnellan <john@codybartfast.com>
2020-12-02 05:48:38 +0000

    Day2 - simplify

--- Day02.fs
+++ Day02.fs
@@ -1,34 +1,20 @@
-open System
 open System.Text.RegularExpressions
 
-let inline toChars (str: string) = str.ToCharArray()
-
 let lines = System.IO.File.ReadAllLines("Day02.txt") |> Array.toList
-let parse (ln: string) = 
-    let m = Regex.Match(ln, @"(\d+)-(\d+) (\w): (.*)")
-    ((m.Groups.[3].Value.[0], (int m.Groups.[1].Value, int m.Groups.[2].Value)),
-        m.Groups.[4].Value)
-let input = lines |> List.map parse
+let parse ln =
+    let [| mn; mx; c; pwd |] = Regex.Split(ln, @"[: -]+")
+    (c.[0], (int mn, int mx)), pwd
+let passwords = lines |> List.map parse
 
-let validDownTheStreet ((c, (mn, mx)), (pwd: string)) =
-    pwd.ToCharArray()
-    |> Array.groupBy id
-    |> Map
-    |> (fun map -> 
-        match Map.tryFind c map with
-        | None -> false
-        | Some arr -> mn <= arr.Length && arr.Length <= mx)
+let validDownTheStreet ((c, (mn, mx)), pwd) =
+    let n = Regex.Matches(pwd, c.ToString()).Count
+    mn <= n && n <= mx
 
 let validHere ((c, (mn, mx)), (pwd: string)) =
-    let a = pwd.[mn - 1] = c
-    let b = pwd.[mx - 1] = c
-    (a && (not b)) || ((not a) && b)
+    pwd.[mn - 1] = c = (pwd.[mx - 1] <> c)
 
-let part1 =
-    input |> List.filter validDownTheStreet  |> List.length
-
-let part2 =
-    input |> List.filter validHere |> List.length
+let part1 = passwords |> List.filter validDownTheStreet |> List.length
+let part2 = passwords |> List.filter validHere |> List.length
 
 [<EntryPoint>]
 let main _ =

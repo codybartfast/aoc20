01bb2177668c7d19da49e0b7d17825e4b22090ef
John Donnellan <john@codybartfast.com>
2020-12-03 16:55:57 +0000

    🛷
    Harder working unfold inspired by aklefdal:
            https://github.com/aklefdal/adventofcode-2020/blob/main/aoc-03.fsx

--- Day03.fs
+++ Day03.fs
@@ -2,19 +2,21 @@ open Grid
 
 let area = System.IO.File.ReadAllLines("Day03.txt") |> Grid.ofLines
 
+// Better use of unfold inspired by aklefdal:
+//     https://github.com/aklefdal/adventofcode-2020/blob/main/aoc-03.fsx
 let ntrees (dx, dy) =
     (0, 0)
-    |> Seq.unfold (fun (x, y) -> Some ((x, y), (x + dx, y + dy)))
-    |> Seq.takeWhile (fun (_, y) -> y < area.Height)
-    |> Seq.map (fun (x, y) -> area.[x % area.Width, y])
-    |> Seq.filter ((=) '#')
-    |> Seq.length
+    |> List.unfold (function
+        | _, y when y >= area.Height -> None
+        | x, y -> Some ((x, y), ((x + dx) % area.Width, y + dy)))
+    |> List.filter (area.Get >> ((=) '#'))
+    |> List.length
 
-let part1 = ntrees (1, 3)
+let part1 = ntrees (3, 1)
 
 let part2 =
     [(1, 1); (3, 1); (5, 1); (7, 1); (1, 2)]
-    |> List.map (ntrees >> int64)
+    |> List.map ntrees
     |> List.reduce (*)
 
 [<EntryPoint>]

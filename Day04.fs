b1163758480989a379be7490545122e118e36586
John Donnellan <john@codybartfast.com>
2020-12-04 06:49:23 +0000

    Day 4 - Tidy

--- Day04.fs
+++ Day04.fs
@@ -3,61 +3,42 @@ open System.Text.RegularExpressions
 
 type Passport = Map<string, string>
 
-let inline toChars (str: string) = str.ToCharArray()
-
-
-let raw = System.IO.File.ReadAllText("Day04.txt")
-let lines = Regex.Split(raw, @"(\s*\n\s*){2,}") |> Array.toList
-let parse (ln: string) = ln //.Split(' ')
-let records = lines |> List.map parse |> List.filter (fun str -> not (Regex.IsMatch(str, @"^\s+$")))
+let txt = System.IO.File.ReadAllText("Day04.txt")
 let passports = 
-    records
-    |> List.map (fun rc -> 
-        Regex.Split(rc, @"[;\s+]+")
-        |> Array.map (fun str -> 
-            str.Split(':')
-            |> (fun [| key; value|] -> (key, value)))
+    Regex.Split(txt, @"\r?\n\r?\n") |> Array.toList    
+    |> List.map (fun record -> 
+        Regex.Split(record, @"\s+")
+        |> Array.map (fun kvp -> kvp.Split(':') |> (fun a -> (a.[0], a.[1])))
         |> Map)
 
-
-let plausible pspt = 
+let triage pspt = 
     match Map.count pspt with
     | 8 -> true
-    | 7 when pspt.ContainsKey "cid" |> not -> true
+    | 7 when not (pspt.ContainsKey "cid") -> true
     | _ -> false
 
-let part1 =
-    passports  |> List.filter plausible |> List.length
-
+let part1 = passports |> List.filter triage |> List.length
 
 let byr (p: Passport) =    
-    match Map.tryFind "byr" p with
-    | None -> false
-    | Some str ->
+    let str = Map.find "byr" p
     match Int32.TryParse str with
     | false, _ -> false
     | true, n -> 1920 <= n && n <= 2002
     
 let iyr (p: Passport) =
-    match Map.tryFind "iyr" p with
-    | None -> false
-    | Some str ->
+    let str = Map.find "iyr" p
     match Int32.TryParse str with
     | false, _ -> false
     | true, n -> 2010 <= n && n <= 2020
     
 let eyr (p: Passport) = 
-    match Map.tryFind "eyr" p with
-    | None -> false
-    | Some str ->
+    let str = Map.find "eyr" p
     match Int32.TryParse str with
     | false, _ -> false
     | true, n -> 2020 <= n && n <= 2030
     
 let hgt (p: Passport) = 
-    match Map.tryFind "hgt" p with
-    | None -> false
-    | Some str ->
+    let str = Map.find "hgt" p
     let m = Regex.Match(str, @"^(\d\d\d?)(cm|in)$")
     if not m.Success then false else
     match m.Groups.[2].Value, m.Groups.[1].Value |> int with
@@ -66,32 +47,26 @@ let hgt (p: Passport) =
     | _ -> false
     
 let hcl (p: Passport) = 
-    match Map.tryFind "hcl" p with
-    | None -> false
-    | Some str ->
+    let str = Map.find "hcl" p
     Regex.IsMatch(str, @"^\#[\da-f]{6,6}$")
     
 let ecl (p: Passport) = 
-    match Map.tryFind "ecl" p with
-    | None -> false
-    | Some str ->
+    let str = Map.find "ecl" p
     Regex.IsMatch(str, @"^(amb|blu|brn|gry|grn|hzl|oth)$")
 
 let pid (p: Passport) = 
-    match Map.tryFind "pid" p with
-    | None -> false
-    | Some str ->
+    let str = Map.find "pid" p
     Regex.IsMatch(str, @"^\d{9,9}$")
 
 let part2 =
     passports  
     |> List.filter 
-        (fun p -> plausible p && byr p && iyr p && eyr p && hgt p &&
+        (fun p -> triage p && byr p && iyr p && eyr p && hgt p &&
             hcl p && ecl p && pid p)
     |> List.length
 
 [<EntryPoint>]
 let main _ =
     part1 |> printfn "Part 1: %A"
-    part2 |> printfn "Part 2: %A"
+    part2 |> printfn "Part 2: %d"
     0

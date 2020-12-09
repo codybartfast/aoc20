04c5acf3d08a3a6a6f863d74fc51fdd4213342aa
John Donnellan <john@codybartfast.com>
2020-12-09 07:21:34 +0000

    Day 9 - tweaks

--- Day09.fs
+++ Day09.fs
@@ -2,31 +2,29 @@ let rec pairCombos = function
     | [] | [_] -> []
     | head::tail -> List.map (fun e -> (head, e)) tail @ pairCombos tail
 
-let input =
-    System.IO.File.ReadAllLines("Day09.txt") |> Array.map int64 |> Array.toList
-let preamble, cipher = input |> List.splitAt 25
+let input = Array.map int64 (System.IO.File.ReadAllLines("Day09.txt"))
+let preamble, cipher = input |> Array.splitAt 25
 
-let invalid pool n =
+let isInvalid pool n =
     pairCombos pool
     |> List.map (fun (a, b) -> a + b)
-    |> (List.contains n)
-    |> (function false -> Some n | _ -> None)
+    |> List.contains n
+    |> function false -> Some n | _ -> None
 
 let findInvalid preamble cipher =
-    (preamble, cipher)
-    ||> List.mapFold(fun pool n -> (invalid pool n), List.tail pool @ [n])
+    (preamble |> Array.toList, cipher)
+    ||> Array.mapFold(fun pool n -> isInvalid pool n, List.tail pool @ [n])
     |> fst
 
 let weakness target cipher =
-    let nums = Array.ofList cipher
-    let len = nums.Length
-    seq { for i in 0 .. len - 2 do for j in i + 1 .. len - 1 do yield (i, j) }
-    |> Seq.map (fun (i, j) -> Array.sub nums i (j - i))
+    let len = Array.length cipher
+    seq { for i in 0 .. len - 2 do for j in i + 1 .. len - 1 do yield i, j }
+    |> Seq.map (fun (i, j) -> Array.sub cipher i (j - i))
     |> Seq.find (Array.reduce (+) >> ((=) target))
-    |> (fun contig -> Array.min contig + Array.max contig)
+    |> fun contig -> Array.min contig + Array.max contig
 
-let part1 = findInvalid preamble cipher |> List.pick id
-let part2 () = weakness part1 (preamble@cipher)
+let part1 = findInvalid preamble cipher |> Array.pick id
+let part2 = weakness part1 input
 
 [<EntryPoint>]
-let main _ = printfn "Part 1: %d" (part1); printfn "Part 2: %A" (part2 ()); 0
+let main _ = printfn "Part 1: %d" part1; printfn "Part 2: %d" part2; 0

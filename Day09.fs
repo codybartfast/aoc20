c8e355cdf8c643f095220fdb635adead5e3fadd8
John Donnellan <john@codybartfast.com>
2020-12-09 11:52:33 +0000

    Day 9 - yyat

--- Day09.fs
+++ Day09.fs
@@ -10,20 +10,18 @@ let isInvalid preamble n =
     |> Array.contains n
     |> function false -> Some n | _ -> None
 
-let findInvalid =
+let part1 =
     (preamble, cipher)
     ||> Array.mapFold(fun preamble n ->
         (isInvalid preamble n), (Array.append preamble.[1..] [|n|]))
     |> fst
+    |> Array.pick id
 
-let weakness target =
+let part2 =
     pairs input.Length
     |> Seq.map (fun (i, j) -> input.[i..j])
-    |> Seq.find (Array.sum >> ((=) target))
+    |> Seq.find (Array.sum >> ((=) part1))
     |> fun contig -> Array.min contig + Array.max contig
-
-let part1 = findInvalid  |> Array.pick id
-let part2 = weakness part1
 
 [<EntryPoint>]
 let main _ = printfn "Part 1: %d" part1; printfn "Part 2: %d" part2; 0

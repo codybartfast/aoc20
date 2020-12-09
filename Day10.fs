da1432523c53f8bf702c7f226f25511edafcba0e
John Donnellan <john@codybartfast.com>
2020-12-09 20:27:50 +0000

    Day 10 - prep

--- Day10.fs
+++ Day10.fs
@@ -4,7 +4,7 @@ open System.Text.RegularExpressions
 let toBlocks (str: string) = Regex.Split(str.Trim(), @"(?:\r?\n){2,}")
 let inline toChars (str: string) = str.ToCharArray()
 
-let lines = System.IO.File.ReadAllLines("Day1.txt")
+let lines = System.IO.File.ReadAllLines("Day10.txt")
 let parse (ln: string) =
     let flds = Regex.Split(ln, @" ")
     flds.[0]

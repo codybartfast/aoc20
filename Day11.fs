b6a1c489cd2335a412e6b9dab6e0ef5f14ff6ad1
John Donnellan <john@codybartfast.com>
2020-12-11 11:55:00 +0000

    Day 11 - refactor

--- Day11.fs
+++ Day11.fs
@@ -1,70 +1,48 @@
-open System
-open System.Text.RegularExpressions
 open Grid
 
-let toBlocks (str: string) = Regex.Split(str.Trim(), @"(?:\r?\n){2,}")
-let inline toChars (str: string) = str.ToCharArray()
-
-let area = System.IO.File.ReadAllLines("Day11.txt") |> Grid.ofLines
 type Area = Grid<char>
 
-let seat1 (area: Area) x y =
-    if area.[x, y] = '.' then '.' else
-    let adj = area.Adjacent (x,y)
-    match adj |> List.map snd |> List.filter ((=) '#') |> List.length with
-    | 0 -> '#'
-    | 1 | 2 | 3 -> area.Get (x, y)
-    | _ -> 'L'
+let area = System.IO.File.ReadAllLines("Day11.txt") |> Grid.ofLines
 
-let round1 (area: Area) = area.Transform seat1
-    
-let part1 () =
+let (++) (x, y) (x', y') = (x + x', y + y')
+
+let considerAdjacent (area: Area) seat v =
+    if area.Get seat = '.' then '.' else
+    area.Adjacent seat |> List.filter (snd >> (=) '#') |> List.length
+    |> function
+        | 0 -> '#'
+        | 1 | 2 | 3 -> v
+        | _ -> 'L'
+
+let shuffle consider (area: Area) =
     area
     |> Seq.unfold (fun area ->
-        let nxt = round1 area
-        if nxt.ToString() = area.ToString() then None else
-        Some (nxt, nxt))
+        let nxt = area.Transform consider
+        if nxt = area then None else Some (nxt, nxt))
     |> Seq.last
     |> Grid.count ((=) '#')
 
-let visSeat (area: Area) (x, y) (dx, dy) =
-    let inbounds (x, y) = (x >= 0 && y >= 0 && x < area.Width && y < area.Height)
-    // let (++) (x, y) (x', y') = (x + x', y + y')
-    let view = (x, y) |> List.unfold (fun (x, y) ->
-        if not (inbounds (x, y)) then None else
-        let nxt = x + dx, y + dy
-        Some ((x, y), nxt))
-    let sts = view.Tail |> List.map area.Get |> List.skipWhile ((=) '.')
-    match sts with
-    | [] -> '.'
-    | h::t -> h
-    
-let adjacent (area: Area) (x, y) =
-    [(0, -1); (1, -1); (1, 0); (1, 1);
-     (0, 1); (-1, 1); (-1, 0); (-1, -1)]
-    |> List.map (visSeat area (x, y))
+let part1 () = shuffle considerAdjacent area
 
-let seat2 (area: Area) x y =
-    if area.[x, y] = '.' then '.' else
-    let adj = adjacent area (x, y)
-    match adj |> List.filter ((=) '#') |> List.length with
-    | 0 -> '#'
-    | 1 | 2 | 3 | 4 -> area.Get (x, y)
-    | _ -> 'L'
+let visible (area: Area) seat dir =
+    seat |> Seq.unfold (fun seat ->
+        let nxt = seat ++ dir
+        if area.InRange nxt then Some (nxt, nxt) else None)
+    |> Seq.map area.Get
+    |> Seq.skipWhile ((=) '.')
+    |> Seq.tryHead
+    |> function None -> '.' | Some h -> h
 
-let round2 (area: Area) = area.Transform seat2
+let considerVisible (area: Area) seat v =
+    if area.Get seat = '.' then '.' else
+    [(0, -1); (1, -1); (1, 0); (1, 1); (0, 1); (-1, 1); (-1, 0); (-1, -1)]
+    |> List.map (visible area seat) |> List.filter ((=) '#') |> List.length
+    |> function
+        | 0 -> '#'
+        | 1 | 2 | 3 | 4 -> v
+        | _ -> 'L'
 
-
-let part2 () =
-//     let full = area |> (round2 >> id)
-//     adjacent full (0, 0)
-    area
-    |> Seq.unfold (fun area ->
-        let nxt = round2 area
-        if nxt.ToString() = area.ToString() then None else
-        Some (nxt, nxt))
-    |> Seq.last
-    |> Grid.count ((=) '#')
+let part2 () = shuffle considerVisible area
 
 [<EntryPoint>]
 let main _ = printfn "Part 1: %A" (part1 ()); printfn "Part 2: %A" (part2 ()); 0

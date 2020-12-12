085429c76ec25ee30be0eb318649cd5c9f18e6d2
John Donnellan <john@codybartfast.com>
2020-12-12 07:36:35 +0000

    🌊

---
+++ Day12.fs
@@ -0,0 +1,35 @@
+type Instr =
+    | Move of (int * int)
+    | Turn of (int * int -> int * int)
+    | Forward of int
+type MoveWhat = Ship | Waypoint
+
+let lines = System.IO.File.ReadAllLines("Day12.txt")
+let parse (ln: string) =
+    match ln.[0], int ln.[1..] with
+    | 'N', n -> Move (0, -n)
+    | 'E', n -> Move (n, 0)
+    | 'S', n -> Move (0, n)
+    | 'W', n -> Move (-n, 0)
+    | 'L', 90 | 'R', 270 -> Turn (fun (x, y) -> (y, -x))
+    | 'R', 90 | 'L', 270 -> Turn (fun (x, y) -> (-y, x))
+    | _, 180 -> Turn (fun (x, y) -> (-x, -y))
+    | 'F', n -> Forward n
+let insts = lines |> Array.map parse |> Array.toList
+
+let (++) (x, y) (x', y') = (x + x', y + y')
+let scale n (x, y) = (n * x, n * y)
+let dist ((x, y), _) = abs x + abs y
+
+let sail moveWhat (ship, waypoint) inst =
+    match moveWhat, inst with
+    | Ship, Move v -> ship ++ v, waypoint
+    | Waypoint, Move v -> ship, waypoint ++ v
+    | _, Turn turn -> ship, turn waypoint
+    | _, Forward n -> (ship ++ (scale n waypoint)), waypoint
+
+let part1 () = (((0, 0), (1, 0)), insts) ||> List.fold (sail Ship) |> dist
+let part2 () = (((0, 0), (10, -1)), insts) ||> List.fold (sail Waypoint) |> dist
+
+[<EntryPoint>]
+let main _ = printfn "Part 1: %A" (part1 ()); printfn "Part 2: %A" (part2 ()); 0

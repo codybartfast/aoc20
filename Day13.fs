d49785eaeb791ddc701c0a8725660b372887940b
John Donnellan <john@codybartfast.com>
2020-12-13 07:44:42 +0000

    🚏

--- Day13.fs
+++ Day13.fs
@@ -12,19 +12,14 @@ let part1 () =
     busses
     |> Seq.map (fun (_, busId) -> (busId, wait busId))
     |> Seq.minBy (snd)
-    |> fun (busId, d) -> busId * d
+    |> fun (busId, wait) -> busId * wait
 
-let rec matchWait deptTime period (busStop, bussId) =
-    match (deptTime + busStop) % bussId with
-    | 0L -> deptTime, period * bussId
-    | _ -> matchWait (deptTime + period) period (busStop, bussId)
+let rec matchWait (depTime, period) (busStop, bussId) =
+    match (depTime + busStop) % bussId with
+    | 0L -> depTime, period * bussId
+    | _ -> matchWait ((depTime + period), period) (busStop, bussId)
 
-let rec winTime (depTime, period) busPairs  =
-    match busPairs with
-    | [] -> depTime
-    | bus::rest -> winTime (matchWait depTime period bus) rest
-
-let part2 () = winTime (0L, 1L) busses
+let part2 () = ((0L, 1L), busses) ||> List.fold matchWait |> fst
 
 [<EntryPoint>]
 let main _ = printfn "Part 1: %d" (part1 ()); printfn "Part 2: %d" (part2 ()); 0

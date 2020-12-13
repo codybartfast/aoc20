941b5c5da8e4631b787455cb3dbdc197c8179375
John Donnellan <john@codybartfast.com>
2020-12-13 07:11:50 +0000

    Day 13 - tidy

--- Day13.fs
+++ Day13.fs
@@ -1,47 +1,30 @@
-open System
-open System.Text.RegularExpressions
-
-let toBlocks (str: string) = Regex.Split(str.Trim(), @"(?:\r?\n){2,}")
-let inline toChars (str: string) = str.ToCharArray()
-
 let lines = System.IO.File.ReadAllLines("Day13.txt")
-let atime = lines.[0] |> int
-let btimes = 
-    Regex.Matches(lines.[1], @"\d+")
-    |> Seq.map(fun m -> m.Value |> int) 
-
-let busses = 
+let arvlTime = lines.[0] |> int64
+let busses =
     lines.[1].Split(',')
-    |> Array.mapi (fun i s -> 
-        match Int64.TryParse(s) with true, n -> Some (int64 i, n) | _ -> None)
+    |> Array.mapi (fun i s -> if s = "x" then None else Some (int64 i, int64 s))
     |> Array.choose id
     |> Array.toList
 
-let wait atime btime = 
-    (btime - (atime % btime)) % btime
+let wait bussId = (bussId - (arvlTime % bussId)) % bussId
 
 let part1 () =
-    btimes
-    |> Seq.map (fun b -> (b, wait atime b))
+    busses
+    |> Seq.map (fun (_, busId) -> (busId, wait busId))
     |> Seq.minBy (snd)
-    |> fun (b, d) -> b * d
+    |> fun (busId, d) -> busId * d
 
-let rec wait2 deptTime period diff bussId =
-    if (deptTime + diff) % bussId = 0L then 
-        deptTime
-    else
-        wait2 (deptTime + period) period diff bussId
+let rec matchWait deptTime period (busStop, bussId) =
+    match (deptTime + busStop) % bussId with
+    | 0L -> deptTime, period * bussId
+    | _ -> matchWait (deptTime + period) period (busStop, bussId)
 
-let rec theTime depTime busPairs period : Int64 =
-    if List.isEmpty busPairs then depTime else
-    let (prevPos, _), (pos, bussId) = busPairs.Head
-    let diff = pos //- prevPos
-    let depTime = wait2 depTime period diff bussId
-    theTime depTime busPairs.Tail (period * bussId)
+let rec winTime (depTime, period) busPairs  =
+    match busPairs with
+    | [] -> depTime
+    | bus::rest -> winTime (matchWait depTime period bus) rest
 
-let part2 () = 
-    let busPairs = (0L, 0L)::busses |> List.pairwise
-    theTime 0L busPairs 1L
+let part2 () = winTime (0L, 1L) busses
 
 [<EntryPoint>]
-let main _ = printfn "Part 1: %A" (part1 ()); printfn "Part 2: %A" (part2 ()); 0
+let main _ = printfn "Part 1: %d" (part1 ()); printfn "Part 2: %d" (part2 ()); 0

0fb45636fcaff8ffa327dc9339e04bf31dff50ba
John Donnellan <john@codybartfast.com>
2020-12-15 12:35:39 +0000

    Day 15 - tweakier

--- Day15.fs
+++ Day15.fs
@@ -1,19 +1,16 @@
-let start =
-    System.IO.File.ReadAllLines("Day15.txt").[0].Split(',') |> Array.map int
+let start = System.IO.File.ReadAllText("Day15.txt").Split(',') |> Array.map int
 
-let game rounds =
+let inline next (seen: int[]) spoken round =
+    let nxt = match seen.[spoken] with 0 -> 0 | prev -> round - prev
+    seen.[spoken] <- round; nxt
+
+let play rounds =
     let seen = Array.zeroCreate rounds
     start.[0..(start.Length - 2)] |> Array.iteri (fun i n -> seen.[n] <- i + 1)
+    (Array.last start, seq{start.Length..(rounds - 1)}) ||> Seq.fold (next seen)
 
-    let rec play spoken round =
-        if round = rounds then spoken else
-        let next = match seen.[spoken] with 0 -> 0 | prev -> round - prev
-        seen.[spoken] <- round
-        play next (round + 1)
-    play (Array.last start) start.Length
-
-let part1 () = game 2020
-let part2 () = game 30_000_000
+let part1 () = play 2020
+let part2 () = play 30_000_000
 
 [<EntryPoint>]
 let main _ = printfn "Part 1: %A" (part1 ()); printfn "Part 2: %A" (part2 ()); 0

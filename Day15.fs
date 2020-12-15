052ada96717aa3588a6025f54005ca3993bf24ad
John Donnellan <john@codybartfast.com>
2020-12-15 07:23:58 +0000

    Day 15 - refactor

--- Day15.fs
+++ Day15.fs
@@ -1,17 +1,19 @@
 let start =
     System.IO.File.ReadAllLines("Day15.txt").[0].Split(',') |> Array.map int
-let startMap =
-    start.[0..(start.Length - 2)] |> Array.mapi (fun i n -> (n, i + 1))  |> Map
 
-let play rounds =
-    let rec play  max last len (seen: Map<int, int>) =
-        if max = (len ) then last else
-        let next = match seen.TryFind last with| None -> 0 | Some n -> len - n
-        play max next (len + 1) (seen.Add (last, len ))
-    play rounds (Array.last start) (start.Length) startMap
+let game rounds =
+    let seen = Array.zeroCreate rounds
+    start.[0..(start.Length - 2)] |> Array.iteri (fun i n -> seen.[n] <- i + 1)
 
-let part1 () = play 2020
-let part2 () = play 30_000_000
+    let rec play prev len =
+        if len = rounds then prev else
+        let next = match seen.[prev] with 0 -> 0 | n -> len - n
+        seen.[prev] <- len
+        play next (len + 1)
+    play (Array.last start) start.Length
+
+let part1 () = game 2020
+let part2 () = game 30_000_000
 
 [<EntryPoint>]
 let main _ = printfn "Part 1: %A" (part1 ()); printfn "Part 2: %A" (part2 ()); 0

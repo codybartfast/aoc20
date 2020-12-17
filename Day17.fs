68a08a05ce69ea5f59585915a3ccf2705726317e
John Donnellan <john@codybartfast.com>
2020-12-17 13:09:54 +0000

    Day 17 - refactor

--- Day17.fs
+++ Day17.fs
@@ -11,7 +11,10 @@ let start =
     lines |> Array.map toChars |> Array.toList
     |> List.mapi (fun y row -> row |> Array.mapi (fun x cube -> ((x, y, 0, 0), cube)))
     |> Seq.collect id
-    |> Map
+    |> Seq.filter (snd >> ((=) '#'))
+    |> Seq.map fst
+    |> Set
+
 let (++) (x, y, z, w) (x', y', z', w') = (x + x', y + y', z + z', w + w')
 
 let adjacentCoords =
@@ -30,16 +33,16 @@ let adjacentCoords =
 let adjacent coord = adjacentCoords |> List.map ((++) coord)
 let nhood coord =  coord::(adjacentCoords |> List.map ((++) coord))
 
-let region space = space |> Map.toSeq |> Seq.collect (fst >> nhood) |> Set
+let region space = space |> Seq.collect nhood |> Seq.distinct
 
-let next (space: Map<Coord, char>) coord =
-    let isActive coord = 
-        match Map.tryFind coord space with | Some '#' -> true | _ -> false
+let next (space: Set<Coord>) coord =
+    let isActive coord =  space.Contains coord 
     let count = adjacent coord  |> List.filter isActive |> List.length
     match isActive coord, count with
-    | true, 2 | true, 3 | false, 3 -> coord, '#'
-    | _ -> coord, '.'
-let generation space = region space |> Seq.map (next space) |> Map
+    | true, 2 | true, 3 | false, 3 -> Some coord
+    | _ -> None
+    
+let generation space = space |> region |> Seq.choose (next space) |> Set
 
 let part1 () =
     start 
@@ -49,10 +52,17 @@ let part1 () =
     |> generation 
     |> generation
     |> generation 
-    |> Map.toSeq |> Seq.filter (snd >> ((=) '#')) |> Seq.length
+    |> Set.count
 
 let part2 () =
     "?"
 
 [<EntryPoint>]
-let main _ = printfn "Part 1: %A" (part1 ()); printfn "Part 2: %A" (part2 ()); 0
+let main _ = 
+    let sw = System.Diagnostics.Stopwatch ()
+    sw.Start ()
+    printfn "Part 1: %A" (part1 ());
+    printfn "Part 2: %A" (part2 ());
+    sw.Stop ()
+    printfn "%f" sw.Elapsed.TotalSeconds
+    0

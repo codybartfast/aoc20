0609a6017767969e554a8da715bcd2cbfe8b2179
John Donnellan <john@codybartfast.com>
2020-12-17 19:31:35 +0000

    Day 17 - tweakier

--- Day17.fs
+++ Day17.fs
@@ -1,46 +1,45 @@
 let gens, mrgn = 6, 7
 let lines = System.IO.File.ReadAllLines("Day17.txt")
-let start =
+let initialActive =
     lines
-    |> Array.mapi (fun y row -> row.ToCharArray () |> Array.mapi (fun x cube ->
-        ((x + mrgn, y + mrgn, mrgn, mrgn), cube)))
+    |> Array.mapi (fun y row -> row.ToCharArray () |> Array.mapi (fun x chr ->
+        ((x + mrgn, y + mrgn, mrgn, mrgn), chr)))
     |> Array.collect id
     |> Array.filter (snd >> ((=) '#'))
     |> Array.map fst
 
-let nhoodCoords =
-    let cartpower lst n =
-        let rec crtpwr n prd =
-            if n = 0 then prd else
-            crtpwr (n - 1) (prd |> Seq.collect (fun tpl ->
-                lst |> Seq.map (fun e -> e::tpl)))
-        crtpwr n (Seq.singleton [])
-    cartpower [0; -1; 1] 4
-    |> Seq.map (fun [x; y; z; w] -> (x, y, z, w)) |> Seq.toArray
+let getIsActive cubes =
+    let len = lines.[0].Length + mrgn + mrgn
+    let sparse = Array4D.zeroCreate len len len len
+    cubes |> Seq.iter (fun (x, y, z, w) -> Array4D.set sparse x y z w true)
+    (fun (x, y, z, w) -> Array4D.get sparse x y z w)
 
+let nhoodCoords =
+    let rec cartpower lst n  prd =
+        if n = 0 then prd else
+        cartpower lst (n - 1) (prd |> Seq.collect (fun tpl ->
+            lst |> Seq.map (fun e -> e::tpl)))
+    cartpower [0; -1; 1] 4 (Seq.singleton [])
+    |> Seq.map (fun [x; y; z; w] -> (x, y, z, w)) |> Seq.toArray
 let (++) (x, y, z, w) (x', y', z', w') = (x + x', y + y', z + z', w + w')
 let nhood coord =  nhoodCoords |> Array.map ((++) coord)
 let adjacent  = nhood >> Array.tail
 let cubesNear coords = coords |> Array.collect nhood |> Array.distinct
 
-let next lookup cube =
-    let count = adjacent cube |> Array.sumBy lookup
-    match lookup cube, count with  1, 2 | 1, 3 | 0, 3 -> 1 | _ -> 0
-
-let getLookup cubes =
-    let len = lines.[0].Length + mrgn + mrgn
-    let zet = Array4D.zeroCreate len len len len
-    cubes |> Seq.iter (fun (x, y, z, w) -> Array4D.set zet x y z w 1)
-    (fun (x, y, z, w) -> Array4D.get zet x y z w)
+let evolveCube isActive cube =
+    let count = adjacent cube |> Array.filter isActive |> Array.length
+    match isActive cube, count with
+    |true, 2 | true, 3 | false, 3 -> true
+    | _ -> false
 
 let evolve4D cubes =
-    cubes |> cubesNear |> Array.filter (next (getLookup cubes) >> ((=) 1))
+    cubes |> cubesNear |> Array.filter (evolveCube (getIsActive cubes))
 let evolve3D = evolve4D >> Array.filter (fun (_, _, _, w) -> w = mrgn)
-
 let repeat fn n = List.init n (fun _ -> fn) |> List.reduce (>>)
+let finalSize evolve = initialActive |> repeat evolve gens |> Seq.length
 
-let part1 () = start |> (repeat evolve3D gens) |> Seq.length
-let part2 () = start |> (repeat evolve4D gens) |> Seq.length
+let part1 () = finalSize evolve3D
+let part2 () = finalSize evolve4D
 
 [<EntryPoint>]
 let main _ = printfn "Part 1: %A" (part1 ()); printfn "Part 2: %A" (part2 ()); 0

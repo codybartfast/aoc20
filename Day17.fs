d8aa32995e2d27dee13801dd695771b43bf80b2f
John Donnellan <john@codybartfast.com>
2020-12-17 16:00:43 +0000

    Day 17 - refactor

--- Day17.fs
+++ Day17.fs
@@ -1,77 +1,46 @@
-open System
-open System.Text.RegularExpressions
-
-type Coord = int * int * int * int
-
-let toBlocks (str: string) = Regex.Split(str.Trim(), @"(?:\r?\n){2,}")
-
+let gens, mrgn = 6, 7
 let lines = System.IO.File.ReadAllLines("Day17.txt")
-let inline toChars (str: string) = str.ToCharArray()
 let start =
-    lines |> Array.map toChars |> Array.toList
-    |> List.mapi (fun y row -> row |> Array.mapi (fun x cube -> ((x, y, 0, 0), cube)))
-    |> Seq.collect id
-    |> Seq.filter (snd >> ((=) '#'))
-    |> Seq.map fst
-    |> Set
-
-let (++) (x, y, z, w) (x', y', z', w') = (x + x', y + y', z + z', w + w')
+    lines
+    |> Array.mapi (fun y row -> row.ToCharArray () |> Array.mapi (fun x cube ->
+        ((x + mrgn, y + mrgn, mrgn, mrgn), cube)))
+    |> Array.collect id
+    |> Array.filter (snd >> ((=) '#'))
+    |> Array.map fst
 
 let nhoodCoords =
-  [ (0, 0, 0, 0);
+    let cartpower lst n =
+        let rec crtpwr n prd =
+            if n = 0 then prd else
+            crtpwr (n - 1) (prd |> Seq.collect (fun tpl ->
+                lst |> Seq.map (fun e -> e::tpl)))
+        crtpwr n (Seq.singleton [])
+    cartpower [0; -1; 1] 4
+    |> Seq.map (fun [x; y; z; w] -> (x, y, z, w)) |> Seq.toArray
 
-    (-1, -1, -1, -1); ( 0, -1, -1, -1); (1, -1, -1, -1);  (-1,  0, -1, -1); ( 0,  0, -1, -1); (1,  0, -1, -1); (-1,  1, -1, -1); ( 0,  1, -1, -1); (1,  1, -1, -1);
-    (-1, -1,  0, -1); ( 0, -1,  0, -1); (1, -1,  0, -1);  (-1,  0,  0, -1); ( 0,  0,  0, -1); (1,  0,  0, -1); (-1,  1,  0, -1); ( 0,  1,  0, -1); (1,  1,  0, -1);
-    (-1, -1,  1, -1); ( 0, -1,  1, -1); (1, -1,  1, -1);  (-1,  0,  1, -1); ( 0,  0,  1, -1); (1,  0,  1, -1); (-1,  1,  1, -1); ( 0,  1,  1, -1); (1,  1,  1, -1);
-  
-    (-1, -1, -1,  0); ( 0, -1, -1,  0); (1, -1, -1,  0);  (-1,  0, -1,  0); ( 0,  0, -1,  0); (1,  0, -1,  0); (-1,  1, -1,  0); ( 0,  1, -1,  0); (1,  1, -1,  0);
-    (-1, -1,  0,  0); ( 0, -1,  0,  0); (1, -1,  0,  0);  (-1,  0,  0,  0);                   (1,  0,  0,  0); (-1,  1,  0,  0); ( 0,  1,  0,  0); (1,  1,  0,  0);
-    (-1, -1,  1,  0); ( 0, -1,  1,  0); (1, -1,  1,  0);  (-1,  0,  1,  0); ( 0,  0,  1,  0); (1,  0,  1,  0); (-1,  1,  1,  0); ( 0,  1,  1,  0); (1,  1,  1,  0);
-  
-    (-1, -1, -1,  1); ( 0, -1, -1,  1); (1, -1, -1,  1);  (-1,  0, -1,  1); ( 0,  0, -1,  1); (1,  0, -1,  1); (-1,  1, -1,  1); ( 0,  1, -1,  1); (1,  1, -1,  1);
-    (-1, -1,  0,  1); ( 0, -1,  0,  1); (1, -1,  0,  1);  (-1,  0,  0,  1); ( 0,  0,  0,  1); (1,  0,  0,  1); (-1,  1,  0,  1); ( 0,  1,  0,  1); (1,  1,  0,  1);
-    (-1, -1,  1,  1); ( 0, -1,  1,  1); (1, -1,  1,  1);  (-1,  0,  1,  1); ( 0,  0,  1,  1); (1,  0,  1,  1); (-1,  1,  1,  1); ( 0,  1,  1,  1); (1,  1,  1,  1) ]
+let (++) (x, y, z, w) (x', y', z', w') = (x + x', y + y', z + z', w + w')
+let nhood coord =  nhoodCoords |> Array.map ((++) coord)
+let adjacent  = nhood >> Array.tail
+let cubesNear coords = coords |> Array.collect nhood |> Array.distinct
 
-let nhood coord =  nhoodCoords |> Seq.map ((++) coord)
-let adjacent  = nhood >> Seq.tail
+let next lookup cube =
+    let count = adjacent cube |> Array.sumBy lookup
+    match lookup cube, count with  1, 2 | 1, 3 | 0, 3 -> 1 | _ -> 0
 
-let region space = space |> Seq.collect nhood |> Seq.distinct
+let getLookup cubes =
+    let len = lines.[0].Length + mrgn + mrgn
+    let zet = Array4D.zeroCreate len len len len
+    cubes |> Seq.iter (fun (x, y, z, w) -> Array4D.set zet x y z w 1)
+    (fun (x, y, z, w) -> Array4D.get zet x y z w)
 
-let next (space: Set<Coord>) coord =
-    let isActive coord =  space.Contains coord 
-    let count = adjacent coord  |> Seq.filter isActive |> Seq.length
-    match isActive coord, count with
-    | true, 2 | true, 3 | false, 3 -> Some coord
-    | _ -> None
-    
-let generation space = space |> region |> Seq.choose (next space) |> Set
+let evolve4D cubes =
+    cubes |> cubesNear |> Array.filter (next (getLookup cubes) >> ((=) 1))
+let evolve3D = evolve4D >> Array.filter (fun (_, _, _, z) -> z = mrgn)
 
-let part1 () =
-    let generation = generation >> Set.filter (function (_, _, _, 0) -> true | _ -> false)
-    start 
-    |> generation
-    |> generation 
-    |> generation
-    |> generation 
-    |> generation
-    |> generation 
-    |> Set.count
+let repeat fn n = List.init n (fun _ -> fn) |> List.reduce (>>)
 
-let part2 () =
-    start 
-    |> generation
-    |> generation 
-    |> generation
-    |> generation 
-    |> generation
-    |> generation 
-    |> Set.count
+let part1 () = start |> (repeat evolve3D gens) |> Seq.length
+let part2 () = start |> (repeat evolve4D gens) |> Seq.length
+
 [<EntryPoint>]
-let main _ = 
-    let sw = System.Diagnostics.Stopwatch ()
-    sw.Start ()
-    printfn "Part 1: %A" (part1 ());
-    printfn "Part 2: %A" (part2 ());
-    sw.Stop ()
-    printfn "%f" sw.Elapsed.TotalSeconds
-    0
+let main _ = printfn "Part 1: %A" (part1 ()); printfn "Part 2: %A" (part2 ()); 0

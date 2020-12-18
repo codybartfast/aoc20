e57917b1eecd2806531ef5503654d6e89534e3e3
John Donnellan <john@codybartfast.com>
2020-12-18 21:03:16 +0000

    Day 19 - prep

---
+++ Day19.fs
@@ -0,0 +1,29 @@
+open System
+open System.Text.RegularExpressions
+
+let toBlocks (str: string) = Regex.Split(str.Trim(), @"(?:\r?\n){2,}")
+let inline toChars (str: string) = str.ToCharArray()
+
+let lines = System.IO.File.ReadAllLines("Day19.txt")
+let parse (ln: string) =
+    let flds = Regex.Split(ln, @" ")
+    flds.[0]
+let input = lines |> Array.map parse |> Array.toList
+
+
+
+let part1 () =
+    input
+
+let part2 () =
+    "?"
+
+[<EntryPoint>]
+// let main _ = printfn "Part 1: %A" (part1 ()); printfn "Part 2: %A" (part2 ()); 0
+let main _ =
+    let sw = System.Diagnostics.Stopwatch ()
+    sw.Start()
+    printfn "Part 1: %A" (part1 ()); printfn "Part 2: %A" (part2 ());
+    sw.Stop()
+    printfn "%f" sw.Elapsed.TotalSeconds
+    0

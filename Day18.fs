7288abcdff9931072cc78fab46f1382b37a701df
John Donnellan <john@codybartfast.com>
2020-12-18 14:54:46 +0000

    🗸

--- Day18.fs
+++ Day18.fs
@@ -1,36 +1,35 @@
-open System
 open System.Text.RegularExpressions
 
-let inline toCharList (str: string) = str.ToCharArray() |> Array.toList
 let lines = System.IO.File.ReadAllLines("Day18.txt")
 
-let rec readNumber digits expr =
-    match expr with
-    | d::rest when Char.IsNumber d -> readNumber (d::digits) rest
-    | _ -> digits |> List.rev |> List.toArray |> String |> int64, expr
+let rec readNumber expr =
+    let digits = Regex.Match(expr, @"^\d+").Value
+    int64 digits, expr.[digits.Length..]
 
 let rec applyOperator acc expr advncd =
     let applyToNextNum op expr =
-        let v, rest = readNumber [] expr
+        let v, rest = readNumber expr
         applyOperator (op acc v) rest advncd
-    match advncd, expr with
-    | _, [] -> acc
-    | _, ' '::'+'::' '::rest -> applyToNextNum (+) rest
-    | false, ' '::'*'::' '::rest -> applyToNextNum (*) rest
-    | true, ' '::'*'::' '::rest -> (acc * evalExpr advncd rest)
+    let opStr, rest = expr.[1..1], expr.[3..]
+    match advncd, opStr with
+    | _, "" -> acc
+    | _, "+" -> applyToNextNum (+) rest
+    | false, "*" -> applyToNextNum (*) rest
+    | true, "*" -> (acc * evalSimple advncd rest)
 
-and evalExpr advncd expr =
-    let num, rest = readNumber [] expr
+and evalSimple advncd expr =
+    let num, rest = readNumber expr
     applyOperator num rest advncd
-let evalStr advncd = toCharList >> (evalExpr advncd)
 
-let rec remParens advncd expr =
+let rec removeParens advncd expr =
     let simplified = Regex.Replace(expr, @"\(([\d +*]+)\)", (fun (m: Match) ->
-        m.Groups.[1].Value |> (evalStr advncd) |> string))
-    if simplified = expr then simplified else remParens advncd simplified
+        m.Groups.[1].Value |> (evalSimple advncd) |> string))
+    if simplified = expr then simplified else removeParens advncd simplified
 
-let part1 () = lines |> Array.sumBy ((remParens false) >> (evalStr false))
-let part2 () = lines |> Array.sumBy ((remParens true) >> (evalStr true))
+let eval advanced =  (removeParens advanced) >> evalSimple (advanced)
+
+let part1 () = lines |> Array.sumBy (eval false)
+let part2 () = lines |> Array.sumBy (eval true)
 
 [<EntryPoint>]
 let main _ = printfn "Part 1: %d" (part1 ()); printfn "Part 2: %d" (part2 ()); 0

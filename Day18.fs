46a87c6e0baa205f9287d8d1ee4f3a0652fbcda3
John Donnellan <john@codybartfast.com>
2020-12-18 06:12:17 +0000

    Day 18 - part 1

--- Day18.fs
+++ Day18.fs
@@ -1,19 +1,44 @@
 open System
 open System.Text.RegularExpressions
+let inline digitToInt (c: char) = int c - int '0' |> int64
+
+type Prev = Num of int64 | Op of (int64 -> int64 -> int64)
 
 let toBlocks (str: string) = Regex.Split(str.Trim(), @"(?:\r?\n){2,}")
 let inline toChars (str: string) = str.ToCharArray()
 
 let lines = System.IO.File.ReadAllLines("Day18.txt")
-let parse (ln: string) =
-    let flds = Regex.Split(ln, @" ")
-    flds.[0]
-let input = lines |> Array.map parse |> Array.toList
+
+let rec readVal digits expr  =
+    match expr with
+    | d::rest when Char.IsNumber d -> 
+        readVal  (d::digits) rest
+    | _ ->
+        if digits.IsEmpty then failwith "Oops" else
+        digits |> List.rev |> List.toArray |> String |> int64, expr
+
+let rec readOp acc expr = 
+    match expr with
+    | [] -> acc
+    | ' '::c::' '::rest ->
+    let op = match c with '+' -> (+) | '*' -> (*) | _ -> failwith "Oh Dear!"
+    let v, rest = readVal [] rest
+    readOp (op acc v) rest
+
+let evalStr ln =
+    let v, rest = ln |> toChars |> Array.toList |> (readVal [])
+    readOp v rest
+
+let rec depParens (str: string) =
+    let simp = Regex.Replace(str, @"\(([\d +*]+)\)", (fun (m: Match) -> 
+        m.Groups.[1].Value |> evalStr |> string))
+    if simp = str then str else depParens simp
 
 
+let part1 () = 
+    lines |> Array.sumBy (depParens >> evalStr) 
 
-let part1 () =
-    input
+    
 
 let part2 () =
     "?"

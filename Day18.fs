b683cb1a615095601c1c421a9df9f72acf72bbd9
John Donnellan <john@codybartfast.com>
2020-12-18 15:45:27 +0000

    🗸

--- Day18.fs
+++ Day18.fs
@@ -6,30 +6,27 @@ let rec readNumber expr =
     let digits = Regex.Match(expr, @"^\d+").Value
     int64 digits, expr.[digits.Length..]
 
-let rec applyOperator acc expr advncd =
+let rec applyOperator advncd acc expr =
     let applyToNextNum op expr =
         let v, rest = readNumber expr
-        applyOperator (op acc v) rest advncd
-    let opStr, rest = expr.[1..1], expr.[3..]
-    match advncd, opStr with
+        applyOperator advncd (op acc v) rest
+    let operator, rest = expr.[1..1], expr.[3..]
+    match advncd, operator with
     | _, "" -> acc
     | _, "+" -> applyToNextNum (+) rest
     | false, "*" -> applyToNextNum (*) rest
-    | true, "*" -> (acc * evalSimple advncd rest)
-
-and evalSimple advncd expr =
-    let num, rest = readNumber expr
-    applyOperator num rest advncd
+    | true, "*" -> (acc * eval advncd rest)
+and eval advncd = readNumber >> fun (num, rest) -> applyOperator advncd num rest
 
 let rec removeParens advncd expr =
-    let simplified = Regex.Replace(expr, @"\(([\d +*]+)\)", (fun (m: Match) ->
-        m.Groups.[1].Value |> (evalSimple advncd) |> string))
+    let simplified = Regex.Replace(expr, @"\(([^()]+)\)", (fun (m: Match) ->
+        m.Groups.[1].Value |> eval advncd |> string))
     if simplified = expr then simplified else removeParens advncd simplified
 
-let eval advanced =  (removeParens advanced) >> evalSimple (advanced)
+let homework advncd =  removeParens advncd >> eval advncd
 
-let part1 () = lines |> Array.sumBy (eval false)
-let part2 () = lines |> Array.sumBy (eval true)
+let part1 () = lines |> Array.sumBy (homework false)
+let part2 () = lines |> Array.sumBy (homework true)
 
 [<EntryPoint>]
 let main _ = printfn "Part 1: %d" (part1 ()); printfn "Part 2: %d" (part2 ()); 0

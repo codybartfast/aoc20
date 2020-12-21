b7281b1057088d607d6f2fcd24cb549ebba96c37
John Donnellan <john@codybartfast.com>
2020-12-21 06:45:18 +0000

    Day 21 - part 1

---
+++ Day21.fs
@@ -0,0 +1,44 @@
+open System
+open System.Text.RegularExpressions
+
+let toBlocks (str: string) = Regex.Split(str.Trim(), @"(?:\r?\n){2,}")
+let toLines (str: string) = Regex.Split(str.Trim(), @"\r?\n") |> Array.toList
+let inline toChars (str: string) = str.ToCharArray()
+
+let lines = System.IO.File.ReadAllLines("Day21.txt")
+let parse (ln: string) =
+    let m = Regex.Match(ln, @"((?<i>\w+) )+\(contains ((?<a>\w+)[, ]*)+")
+    let ingrds = m.Groups.["i"].Captures |> Seq.map (fun c -> c.Value) |> Set
+    let allgns = m.Groups.["a"].Captures |> Seq.map (fun c -> c.Value) |> Set
+    ingrds, allgns
+let foods = lines |> Array.map parse |> Array.toList
+
+let ingredients = foods |> List.map fst |> Set.unionMany
+let allergens = foods |> List.map snd |> Set.unionMany
+let informtion = allergens |> Seq.map (fun a -> (a, ingredients)) |> Map
+let candidates = fst
+let excluded = snd
+
+let analyse info (ingrds, allgns) =
+    (info, allgns)
+    ||> Seq.fold( fun info allgn ->
+        Map.add allgn (Set.intersect info.[allgn] ingrds) info)
+    
+let firstpass = (informtion, foods) ||> List.fold analyse
+let suspect =  firstpass |> Map.toSeq |> Seq.map snd |> Set.unionMany
+let free = foods |> List.map (fst >> (fun ingrds -> Set.difference ingrds suspect))
+
+let part1 () = free |> Seq.sumBy Set.count
+
+let part2 () =
+    allergens
+
+[<EntryPoint>]
+// let main _ = printfn "Part 1: %A" (part1 ()); printfn "Part 2: %A" (part2 ()); 0
+let main _ =
+    let sw = System.Diagnostics.Stopwatch ()
+    sw.Start()
+    printfn "Part 1: %A" (part1 ()); printfn "Part 2: %A" (part2 ());
+    sw.Stop()
+    printfn "%f" sw.Elapsed.TotalSeconds
+    0

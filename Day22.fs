05448bbe2a78c2eb21ed42669430452b9c324843
John Donnellan <john@codybartfast.com>
2020-12-22 17:44:32 +0000

    Day 22 - part 2 not crap

--- Day22.fs
+++ Day22.fs
@@ -1,41 +1,48 @@
-#nowarn "25"
-
 open System.Text.RegularExpressions
+
+type Players = Player1 | Player2
 
 let toBlocks (str: string) = Regex.Split(str.Trim(), @"(?:\r?\n){2,}")
 let toLines (str: string) = Regex.Split(str.Trim(), @"\r?\n") |> Array.toList
-let inline toChars (str: string) = str.ToCharArray()
 
-let blocks = System.IO.File.ReadAllText("Day22.txt") |> toBlocks
-let cards txt = txt |> toLines |> List.tail |> List.map int64
-let [| cards1; cards2 |] = blocks |> Array.map cards  
+let input =
+    System.IO.File.ReadAllText("Day22.txt") |> toBlocks
+    |> Array.map (toLines >> List.tail >> List.map int)
+let decks = (input.[0], input.[1])
 
-let move (c1::rest1) (c2::rest2) = rest1, rest2 @ [c2; c1]
+let takeCards winner (deck1, deck2) =
+    let move (c1::rest1) (c2::rest2) = rest1, rest2 @ [c2; c1]
+    match winner with
+    | Player1 ->
+        let cards2, cards1 = move deck2 deck1
+        cards1, cards2
+    | Player2 -> move deck1 deck2
 
-let simpRound ((c1::_ as cards1), (c2::_ as cards2)) =
-    if c1 > c2 then 
-        let d2, d1 = move cards2 cards1
-        d1, d2
-    else 
-        move cards1 cards2
+let simpRound (((c1::_), (c2::_)) as decks) =
+    takeCards (if c1 > c2 then Player1 else Player2) decks
 
-let rec simpPlay = function 
-    [], deck -> deck | deck, [] -> deck | decks -> simpRound decks |> simpPlay
+let rec simpPlay = function
+    | [], deck | deck, [] -> deck
+    | decks -> simpRound decks |> simpPlay
 
-let rec score =
-    List.rev >> List.mapi (fun i card -> (i + 1 |> int64) * card) >> List.sum
-    
-let part1 () = simpPlay (cards1, cards2) |> score
+let rec score = List.rev >> List.mapi (fun i card -> (i + 1) * card) >> List.sum
 
-let part2 () =
-    "?"
+let part1 () = simpPlay decks |> score
+
+let rec round ((c1::rest1 as deck1), (c2::rest2) as decks) hist =
+    if Set.contains decks hist then Player1, deck1 else
+    let hist = Set.add decks hist
+    let winner =
+        if c1 > rest1.Length || c2 > rest2.Length then
+            if c1 > c2 then Player1 else Player2
+        else
+            round (List.take c1 rest1, List.take c2 rest2) hist |> fst
+    match takeCards winner decks with
+    | deck1, [] -> Player1, deck1
+    | [], deck2 -> Player2, deck2
+    | decks -> round decks hist
+
+let part2 () = round decks Set.empty |> snd |> score
 
 [<EntryPoint>]
-// let main _ = printfn "Part 1: %A" (part1 ()); printfn "Part 2: %A" (part2 ()); 0
-let main _ =
-    let sw = System.Diagnostics.Stopwatch ()
-    sw.Start()
-    printfn "Part 1: %A" (part1 ()); printfn "Part 2: %A" (part2 ());
-    sw.Stop()
-    printfn "%f" sw.Elapsed.TotalSeconds
-    0
+let main _ = printfn "Part 1: %A" (part1 ()); printfn "Part 2: %A" (part2 ()); 0

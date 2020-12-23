a3aff32303a96a33a20c2b5280213bc7dac2f307
John Donnellan <john@codybartfast.com>
2020-12-23 07:02:39 +0000

    Day 23 - part 1

--- Day23.fs
+++ Day23.fs
@@ -1,20 +1,61 @@
 open System
 open System.Text.RegularExpressions
+open Ring
+
 
 let toBlocks (str: string) = Regex.Split(str.Trim(), @"(?:\r?\n){2,}")
 let toLines (str: string) = Regex.Split(str.Trim(), @"\r?\n") |> Array.toList
 let inline toChars (str: string) = str.ToCharArray()
+let inline digitToInt (c: char) = int c - int '0'
 
-let lines = System.IO.File.ReadAllLines("Day23.txt")
-let parse (ln: string) =
-    let flds = Regex.Split(ln, @" ")
-    flds.[0]
-let input = lines |> Array.map parse |> Array.toList
+let input = 
+    System.IO.File.ReadAllLines("Day23.txt").[0]
+    |> toChars |> Array.map digitToInt |> Array.toList
+    
+let start () = input |> Ring.ofList
+let sorted = input |> List.sortDescending |> Ring.ofList
 
+let remove game =
+    let rec remove n rmvd game =
+        if n = 0 then game, List.rev rmvd else
+        let trgt = Ring.item game
+        match Ring.remove game with
+        | EmptyRing -> failwith "oops"
+        | game -> remove (n - 1) (trgt::rmvd) game
+    let game = Ring.next game
+    let game, rmvd = remove 3 [] game
+    (Ring.prev game, rmvd)
 
+let goDestination game rmvd =
+    let rec destination srtd =
+        // printfn "Checking for %A " (Ring.item srtd)
+        if List.contains (Ring.item srtd) rmvd then 
+            destination (Ring.next srtd) 
+        else
+            Ring.item srtd
+    let srtd = sorted |> Ring.find (Ring.item game) |> Ring.next
+    Ring.find (destination srtd) game
+
+let rec insert game rmvd =
+    (game, rmvd) ||> List.fold (fun g r -> Ring.insert r g)
+
+let move game =    
+    let game, rmvd = remove game
+    let crnt = Ring.item game
+    let game = goDestination game rmvd
+    let game = insert game rmvd
+    game |> Ring.find crnt |> Ring.next
+
+let rec play n game =
+    if n = 0 then game else play (n - 1) (move game)
+
+let final game = 
+    game 
+    |> Ring.find 1 |> Ring.toList |> List.tail  |> List.map (fun c -> c.ToString() ) |>  String.concat ""
 
 let part1 () =
-    input
+    play 100 (start ()) |> final
+
 
 let part2 () =
     "?"

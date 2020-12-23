2a28179ab9b10a6ad8101a023b3b12b371f37564
John Donnellan <john@codybartfast.com>
2020-12-23 11:40:24 +0000

    Day 23 - tweak

--- Day23.fs
+++ Day23.fs
@@ -10,46 +10,46 @@ let seed =
     |> toChars |> Array.map digitToInt |> Array.toList
 let labels size = seed @ [seed.Length + 1 .. size]
 
-let getShell shellArr v =
+let getCup cupArr v =
     let index =
         if v <= seed.Length then List.findIndex ((=) v) seed else (v - 1)
-    Array.item index shellArr
+    Array.item index cupArr
 
-let removeAfter shell =
-    let rmvd = shell.Next
+let removeAfter cup =
+    let rmvd = cup.Next
     let joinTo = rmvd.Next.Next.Next
-    shell.Next <- joinTo; joinTo.Prev <- shell; rmvd
+    cup.Next <- joinTo; joinTo.Prev <- cup; rmvd
 
-let getDestination shellArr shell rmvd =
-    let inline next n = if n = 1 then Array.length shellArr else n - 1
+let getDestination cupArr cup rmvd =
+    let inline next n = if n = 1 then Array.length cupArr else n - 1
     let rec checkDest n = if List.contains n rmvd then checkDest (next n) else n
-    let dest = checkDest (Ring.item shell |> next)
-    getShell shellArr dest
+    let dest = checkDest (Ring.item cup |> next)
+    getCup cupArr dest
 
-let rec insertAfter shell rmvd =
-    let joinTo = shell.Next
-    shell.Next <- rmvd; rmvd.Prev <- shell;
+let rec insertAfter cup rmvd =
+    let joinTo = cup.Next
+    cup.Next <- rmvd; rmvd.Prev <- cup;
     rmvd.Next.Next.Next <- joinTo; joinTo.Prev <- rmvd.Next.Next
 
-let round shellArr shell =
-    let rmvd = removeAfter shell
+let round cupArr cup =
+    let rmvd = removeAfter cup
     let rmvdVals = [rmvd.Item; rmvd.Next.Item; rmvd.Next.Next.Item]
-    insertAfter (getDestination shellArr shell rmvdVals) rmvd
-    shell |> Ring.next
+    insertAfter (getDestination cupArr cup rmvdVals) rmvd
+    cup |> Ring.next
 
 let play size rounds =
     let start = (labels size) |> Ring.ofList
-    let shellArr = start |> Ring.toLinks |> List.toArray
-    (start, seq{1 .. rounds}) ||> Seq.fold (fun game _ -> round shellArr game)
-    getShell shellArr 1
+    let cupArr = start |> Ring.toLinks |> List.toArray
+    (start, seq{1 .. rounds}) ||> Seq.fold (fun game _ -> round cupArr game)
+    getCup cupArr 1
 
 let part1 () =
     play seed.Length 100 |> Ring.toList |> List.tail
     |> List.map intToDigit  |> List.toArray |> String |> int
 
 let part2 () =
-    let shell1 = play 1_000_000 10_000_000
-    (int64 shell1.Next.Item) * (int64 shell1.Next.Next.Item)
+    let cup1 = play 1_000_000 10_000_000
+    (int64 cup1.Next.Item) * (int64 cup1.Next.Next.Item)
 
 [<EntryPoint>]
 let main _ = printfn "Part 1: %d" (part1 ()); printfn "Part 2: %d" (part2 ()); 0

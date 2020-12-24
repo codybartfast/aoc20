6b5e64cb8ad28bd016372465c11ced020be0cd40
John Donnellan <john@codybartfast.com>
2020-12-24 09:50:51 +0000

    Day 24 - part 1

--- Day24.fs
+++ Day24.fs
@@ -1,21 +1,40 @@
 open System
 open System.Text.RegularExpressions
 
+let (++) (x, y) (x', y') = (x + x', y + y')
+
 let toBlocks (str: string) = Regex.Split(str.Trim(), @"(?:\r?\n){2,}")
 let toLines (str: string) = Regex.Split(str.Trim(), @"\r?\n") |> Array.toList
 let inline toChars (str: string) = str.ToCharArray()
 
+let e, se, sw, w, nw, ne = (2, 0), (1 ,1),(-1, 1), (-2, 0), (-1, -1), (1, -1)
+let dirs = [e; se; sw; w; nw; ne]
+
 let lines = System.IO.File.ReadAllLines("Day24.txt")
 let parse (ln: string) =
-    let flds = Regex.Split(ln, @" ")
-    flds.[0]
-let input = lines |> Array.map parse |> Array.toList
+    let cs = Regex.Matches(ln, @"(e|se|sw|w|nw|ne)")
+    cs |> Seq.map (fun c -> c.Value) |> Seq.toList
+    |> List.map (function
+        | "e" -> e | "se" -> se | "sw" -> sw
+        | "w" -> w | "nw" -> nw | "ne" -> ne)
+let tile path =
+    ((0, 0), path) ||> List.fold (++)
+let paths = lines |> Array.map parse |> Array.toList
+let isBlack n = n % 2 = 1
 
+let flip floor crd = 
+    let tile = 
+        match Map.tryFind crd floor with 
+        | None | Some false -> true
+        | Some true -> false
+    floor.Add (crd, tile)
 
+let flipPath floor path = flip floor (tile path)
 
 let part1 () =
-    input
-
+    (Map.empty, paths) ||> List.fold flipPath 
+    |> Map.toSeq |> Seq.filter snd |> Seq.length
+        
 let part2 () =
     "?"
 

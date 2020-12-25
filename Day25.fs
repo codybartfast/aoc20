f6c1ac812e60c014482e38757a337e61d42a446c
John Donnellan <john@codybartfast.com>
2020-12-25 07:56:48 +0000

    Day 25 - tidied

--- Day25.fs
+++ Day25.fs
@@ -1,38 +1,10 @@
-open System
-open System.Text.RegularExpressions
+let [|card; door|] = System.IO.File.ReadAllLines("Day25.txt") |> Array.map int64
 
-let toBlocks (str: string) = Regex.Split(str.Trim(), @"(?:\r?\n){2,}")
-let toLines (str: string) = Regex.Split(str.Trim(), @"\r?\n") |> Array.toList
-let inline toChars (str: string) = str.ToCharArray()
+let next prev subj = (prev * subj) % 20201227L
+let rec loop acc enc =
+        if acc = door then enc else loop (next acc 7L) (next enc card)
 
-let lines = System.IO.File.ReadAllLines("Day25.txt")
-let cardPub = int64 lines.[0]
-let doorPub = int64 lines.[1]
-
-let next p subj = (p * subj) % 20201227L
-
-let search pub =
-    printfn "Searching for %d" pub
-    let rec loop l p =
-        let r = next p 7L
-        if r = pub then l else loop (l + 1L) r
-    loop 1L 1L
-
-
-
-let part1 () =
-    let cardLoop, doorLoop = search cardPub, search doorPub
-    (cardPub, seq{2L .. doorLoop}) ||> Seq.fold (fun p _ -> next p cardPub)
-
-let part2 () =
-    "?"
+let part1 = loop (next 1L 7L) card
 
 [<EntryPoint>]
-// let main _ = printfn "Part 1: %A" (part1 ()); printfn "Part 2: %A" (part2 ()); 0
-let main _ =
-    let sw = System.Diagnostics.Stopwatch ()
-    sw.Start()
-    printfn "Part 1: %A" (part1 ()); printfn "Part 2: %A" (part2 ());
-    sw.Stop()
-    printfn "%f" sw.Elapsed.TotalSeconds
-    0
+let main _ = printfn "Part 1: %d" part1; printfn "Part 2: Merry Christmas!"; 0

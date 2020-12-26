329a8cf346a5761d1d1d84e38897ea9f7f0c2d7b
John Donnellan <john@codybartfast.com>
2020-12-26 08:28:41 +0000

    Merry Christmas!

--- Day25.fs
+++ Day25.fs
@@ -1,10 +1,19 @@
 let [|card; door|] = System.IO.File.ReadAllLines("Day25.txt") |> Array.map int64
 
-let next prev subj = (prev * subj) % 20201227L
-let rec loop acc enc =
-        if acc = door then enc else loop (next acc 7L) (next enc card)
+let itr n subj = (n * subj) % 20201227L
+let rec loop trial key =
+        if trial = door then key else loop (itr trial 7L) (itr key card)
 
-let part1 = loop (next 1L 7L) card
+let part1 = loop (itr 1L 7L) card
 
 [<EntryPoint>]
 let main _ = printfn "Part 1: %d" part1; printfn "Part 2: Merry Christmas!"; 0
+
+(* Golfier
+
+let [|c; d|] = System.IO.File.ReadAllLines("Day25.txt") |> Array.map int64
+let i n s = n * s % 20201227L
+let rec l t k = if t = d then k else l (i t 7L) (i k c)
+l (i 1L 7L) c
+
+*)
